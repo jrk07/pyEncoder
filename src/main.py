6fff781c4ff413656180960e8e63785a06b04c5a
Justin <justinkennedy07@Mac.attlocal.net>
2024-10-14 13:45:40 -0700

    refactor to simplify and implement error handling

--- src/main.py
+++ src/main.py
@@ -1,53 +1,66 @@
-from ffmpeg import FFmpeg, Progress
+from ffmpeg import FFmpeg, FFmpegError, FFmpegUnsupportedCodec, Progress
 from os import getenv
 from pathlib import Path
 
-# Grab the path to the video we want to encode from environment.
-path_from_env = getenv('VIDEO_TO_ENCODE')
+# Grab the path to the video we want to encode.
+input_video_path = Path(getenv('VIDEO_TO_ENCODE'))
 
-# Ensure the VIDEO_TO_ENCODE environment variable exists, if not, exit and log. 
-if path_from_env:
-    input_video_path = Path(path_from_env)
-else: 
-    print("VIDEO_TO_ENCODE environment variable not set,  exiting.")
+if not input_video_path:
+    print("VIDEO_TO_ENCODE environment variable not set, exiting.")
     exit()
 
-# Reuse the input vidoes path and append with x264 to show it's the encoded file along
+# Provide a codec, otherwise default to libx264.
+codec = getenv('CODEC') or "libx264"
+
+# Provide a file suffix, otherwise default to the codec name.
+filename_suffix = getenv('FILE_SUFFIX') or codec
+
+# Reuse the input vidoes path and append with the codec name to show it's the encoded file along
 # with the appropriate extension.
-output_video_path =  input_video_path.parent / (input_video_path.stem + "_x264.mp4")
+
+# TODO custom output path
+output_video_path =  input_video_path.parent / (input_video_path.stem + "_" + filename_suffix + ".mp4")
 
 # Quality can be set if desired, else we default to 18 which is
 # a good balance of quality and filesize. 
 quality = getenv('QUALITY')
-if quality == "LOW":
-    crf_value = 38
-elif quality == "MAX":
-    crf_value = 0
-else: 
-    crf_value = 18
+crf_value = {
+    "LOW": 38,
+    "MAX": 0
+}.get(quality, 18)
 
 def main():
-    ffmpeg = (
-        FFmpeg()
-        .option("y")
-        .input(input_video_path)
-        .output(
-            output_video_path,
-            {"codec:v": "libx264"},
-            crf={crf_value},
+    try:
+        ffmpeg = (
+            FFmpeg()
+            .option("y")
+            .input(input_video_path)
+            .output(
+                output_video_path,
+                {"codec:v": {codec}},
+                crf={crf_value}, 
+            )
         )
-    )
 
-    @ffmpeg.on("progress")
-    def on_progress(progress: Progress):
-        print(progress)
+        @ffmpeg.on("progress")
+        def on_progress(progress: Progress):
+            print(progress)
 
-    @ffmpeg.on("completed")
-    def on_completed():
-        print(f"completed encoding file {output_video_path}")
+        @ffmpeg.on("completed")
+        def on_completed():
+            print(f"completed encoding file {output_video_path}")
 
-    ffmpeg.execute()
+        ffmpeg.execute()
 
+    except FFmpegUnsupportedCodec as exception:
+        print(
+            "Unsupported codec provided, please use a valid codec or remove codec flag to default to x264."
+        )
+
+    except FFmpegError as exception:
+        print("Error running ffmpeg")
+        print("Exception:", exception.message)
+        print("Arguments used to execute ffmpeg:", exception.arguments)
 
 if __name__ == "__main__":
     main()
